bdfc1b75afaef417653344d45d9455c5e208ddf0
B3none <ablackham2000@gmail.com>
2019-03-09 15:58:06 +0000

    Updated example to show all uses.

--- example.js
+++ example.js
@@ -1,3 +1,15 @@
 const steamGroup = require('./index');
 
-steamGroup.getMembers('https://steamcommunity.com/groups/voidrealitygaming');
+let steamGroupUrl = 'https://steamcommunity.com/groups/voidrealitygaming';
+
+// Get a list of all the steam group members.
+steamGroup.getMembers(steamGroupUrl)
+    .then(members => console.log(members));
+
+// Grab a list of members on a specific page (default: 1)
+steamGroup.getPageMembers(steamGroupUrl, 2)
+    .then(pageMembers => console.log(pageMembers));
+
+// Find a member in the steam group.
+steamGroup.findMember(steamGroupUrl, '76561198028510846')
+    .then(isMember => console.log(isMember));
